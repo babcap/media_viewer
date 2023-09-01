13c10625debe0f9305a640f67460328ed3ef5a8f
Arthur Batyaev <takerarthur@gmail.com>
2023-09-01 07:16:25 +0200

    Create project

---
+++ Media_viewer/Media_viewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Media_viewer
+//
+//  Created by Arthur on 01.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
